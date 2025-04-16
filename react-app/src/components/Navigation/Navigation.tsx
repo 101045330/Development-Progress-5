eeb842a216efc44a8f378666818c070eb4ccb225
101045330 <101045330@georgebrown.ca>
2025-04-16 12:23:49 +0000

    nav bg moved below nav like was during the design phase

--- react-app/src/components/Navigation/Navigation.tsx
+++ react-app/src/components/Navigation/Navigation.tsx
@@ -5,20 +5,20 @@ import './Navigation.css';
 const Navigation: React.FC = () => {
   return (
     <header>
-    <nav id="nav">
-      <ul id="main_nav">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/#about">About</Link></li>
-        <li className="mTopHalf">
-          <Link to="/">
-            <img src="/jpg/logo.jpg" alt="Logo" className="logo" />
-          </Link>
-        </li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/helpdesk">Help Desk</Link></li>
-      </ul>
+      <nav id="nav">
+        <ul id="main_nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/#about">About</Link></li>
+          <li className="mTopHalf">
+            <Link to="/">
+              <img src="/jpg/logo.jpg" alt="Logo" className="logo" />
+            </Link>
+          </li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/helpdesk">Help Desk</Link></li>
+        </ul>
+      </nav>
       <div className="background-image"></div>
-    </nav>
     </header>
   );
 };
